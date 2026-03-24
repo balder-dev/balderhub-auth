f97efd26c33d75b0d07febf092b25b7acb3c8cdd
matosys <max@matosys.de>
2026-03-24 17:05:01 +0100

    feat(scenario_features/client): introduce new feature `OperationHandlingFeature`

--- src/balderhub/auth/lib/scenario_features/client/__init__.py
+++ src/balderhub/auth/lib/scenario_features/client/__init__.py
@@ -4,6 +4,7 @@ from .authentification_feature import AuthenticationFeature
 from .authentification_state_machine import AuthenticationStateMachine
 from .has_permissions_for_config import HasPermissionsForConfig
 from .is_unauthenticated_feature import IsUnauthenticatedFeature
+from .operation_handling_feature import OperationHandlingFeature
 from .user_login_feature import UserLoginFeature
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     'AuthenticationStateMachine',
     'HasPermissionsForConfig',
     'IsUnauthenticatedFeature',
+    'OperationHandlingFeature',
     'UserLoginFeature'
 ]
