54e695c80c22e229068d7f1a92a70ab5d9121acf
matosys <max@matosys.de>
2026-03-24 16:58:19 +0100

    feat(scenario_features/client): introduce new feature `IsUnauthenticatedFeature`

--- src/balderhub/auth/lib/scenario_features/client/__init__.py
+++ src/balderhub/auth/lib/scenario_features/client/__init__.py
@@ -1,9 +1,11 @@
 from . import role
 
 from .has_permissions_for_config import HasPermissionsForConfig
+from .is_unauthenticated_feature import IsUnauthenticatedFeature
 from .user_login_feature import UserLoginFeature
 
 __all__ = [
     'HasPermissionsForConfig',
+    'IsUnauthenticatedFeature',
     'UserLoginFeature'
 ]
---
+++ src/balderhub/auth/lib/scenario_features/client/is_unauthenticated_feature.py
@@ -0,0 +1,4 @@
+import balder
+
+class IsUnauthenticatedFeature(balder.Feature):
+    """autonomous feature defining that the device is an unauthenticated device"""
