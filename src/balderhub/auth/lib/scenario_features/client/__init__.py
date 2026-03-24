5ecf652bb607bd6997a20302203f36dd12de67ab
matosys <max@matosys.de>
2026-03-24 17:03:56 +0100

    feat(scenario_features/client): introduce new feature `AuthenticationStateMachine`

--- src/balderhub/auth/lib/scenario_features/client/__init__.py
+++ src/balderhub/auth/lib/scenario_features/client/__init__.py
@@ -1,10 +1,12 @@
 from . import role
 
+from .authentification_state_machine import AuthenticationStateMachine
 from .has_permissions_for_config import HasPermissionsForConfig
 from .is_unauthenticated_feature import IsUnauthenticatedFeature
 from .user_login_feature import UserLoginFeature
 
 __all__ = [
+    'AuthenticationStateMachine',
     'HasPermissionsForConfig',
     'IsUnauthenticatedFeature',
     'UserLoginFeature'
